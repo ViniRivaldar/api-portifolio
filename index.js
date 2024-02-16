477e1c52c1e86eddd83cfcc94a0f76bfeaf04fa2
ViniRivaldar <vinicius-rivaldar@outlook.com>
2024-02-15 21:46:39 -0300

    atualizando

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = process.env.PORT 
 const mongoose = require('mongoose')
 const admin = require('./routes/Admin')
 const bodyParser = require('body-parser')
 const ejs = require('ejs')
 const path = require('path')
-const db = process.env.MONGODB_URL
 const cors = require('cors')
 
 
@@ -26,7 +24,7 @@ app.set('views', path.join(__dirname,'views'))
 
 
 mongoose.Promisse = global.Promisse
-mongoose.connect(db).then(()=>{
+mongoose.connect(process.env.MONGODB_URL).then(()=>{
     console.log(`mongo connected...`)
 }).catch((err)=>{
     console.log(`erro de ${err}`)
@@ -38,7 +36,7 @@ app.use('/', admin)
 
 
 
-app.listen(port,() => {
-    console.log(`server run on a port ${port}`)
+app.listen(process.env.PORT ,() => {
+    console.log(`server run on a port ${process.env.PORT }`)
 })
 
